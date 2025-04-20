5b1c233f40f7aa9e5b5a07087d0758cf6f7c49fb
Merajul Hasan <merajulhasan09@gmail.com>
2025-04-20 16:34:40 +0600

    start working

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+const logger = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next(); // পরবর্তী middleware বা রুটে যাও
+};
+
+app.use(logger); // Globally apply middleware
+
+const checkToken = (req, res, next) => {
+  const token = req.headers.authorization;
+  if (token === "12345") {
+    next();
+  } else {
+    res.status(401).send("Unauthorized");
+  }
+};
+
+app.get("/protected", checkToken, (req, res) => {
+  res.send("You are authorized!");
+});
+
+app.get("/test", logger, checkToken, (req, res) => {
+  res.send("Test successful!");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the homepage");
+});
+
+app.post("/users", (req, res) => {
+  res.send("user created successfully");
+});
+
+app.get("/product/:productId", (req, res) => {
+  const productId = req.params.productId;
+  res.send(`Product ID is: ${productId}`);
+});
+
+// URL: /search?name=book&category=education
+app.get("/search", (req, res) => {
+  const { name, category } = req.query;
+  res.send(`Search result for ${name} in ${category}`);
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
